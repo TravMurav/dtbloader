71b42f887bf2659df24c390f20594a8601fb28ba
Nikita Travkin <nikita@trvn.ru>
2024-05-30 10:14:15 +0500

    describe_hw: Use getopts

--- scripts/describe_hw.sh
+++ scripts/describe_hw.sh
@@ -2,11 +2,47 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Nikita Travkin <nikita@trvn.ru>
 
-if [ $# -eq 1 ]
+dtb_name="FIXME!"
+hwids_file=""
+
+usage() {
+	echo "Usage: $0 [-h] [-d DTB_NAME] [-f FILE]"
+	echo "Generate hardware description struct for dtbloader."
+	echo
+	echo "  -d DTB	Set the name of the DTB to use."
+	echo "  -f FILE	Read FILE as fwupdtool hwids output."
+	echo "  -h		This help."
+	echo
+	echo "By default this tool attempts to survey the device it was run on."
+	echo
+}
+
+while getopts ":d:f:h" opt
+do
+	case $opt in
+		d)
+			dtb_name="$OPTARG"
+			;;
+		f)
+			hwids_file="$OPTARG"
+			;;
+		h)
+			usage
+			exit 0 ;;
+		*)
+			usage
+			echo "Unkown option: -$OPTARG"
+			echo
+			exit 1 ;;
+	esac
+done
+shift $((OPTIND-1))
+
+if [ -z "$hwids_file" ]
 then
-	hwids_data="$(cat "$1")"
-else
 	hwids_data="$(fwupdtool hwids)"
+else
+	hwids_data="$(cat "$hwids_file")"
 fi
 
 get_field() {
@@ -32,7 +68,7 @@ echo "};"
 echo
 echo "static struct device ""$var_prefix""_dev = {"
 echo "	.name  = L\"$(get_field "Manufacturer") $(get_field "Family")\","
-echo "	.dtb   = L\"FIXME!\","
+echo "	.dtb   = L\"$(echo "$dtb_name" | sed 's_[/\\]_\\\\_')\","
 echo "	.hwids = ""$var_prefix""_hwids,"
 echo "};"
 echo "DEVICE_DESC(""$var_prefix""_dev);"
