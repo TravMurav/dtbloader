3c55487f7447a406d83799c689633a6b78a34dd9
Nikita Travkin <nikita@trvn.ru>
2024-08-24 13:18:43 +0500

    scripts: Filter out duplicates in supported dtb list

--- scripts/get_supported_dtbs.sh
+++ scripts/get_supported_dtbs.sh
@@ -2,6 +2,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2024 Nikita Travkin <nikita@trvn.ru>
 
-cd "$(dirname $0)/.." 
+cd "$(dirname $0)/.."
 
-grep -h -E ".dtb +=" src/devices/* | sed -e 's/.*L"\(.*\)",/\1/' -e 's_\\\\_/_' | grep -v Tentative
+grep -h -E ".dtb +=" src/devices/* \
+	| sed -e 's/.*L"\(.*\)",/\1/' -e 's_\\\\_/_' \
+	| grep -v Tentative \
+	| sort -u
