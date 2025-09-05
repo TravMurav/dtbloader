002323e5de9df7de1f25c49cf422b1ae5aee7f88
Nikita Travkin <nikita@trvn.ru>
2025-09-05 22:09:28 +0500

    devices: lenovo_tb16: Remove tentative note

--- src/devices/lenovo_tb16.c
+++ src/devices/lenovo_tb16.c
@@ -27,7 +27,7 @@ static EFI_GUID lenovo_thinkbook_16_g7_qoy_hwids[] = {
 
 static struct device lenovo_thinkbook_16_g7_qoy_dev = {
 	.name  = L"LENOVO ThinkBook 16 G7 QOY",
-	.dtb   = L"qcom\\x1p42100-lenovo-thinkbook-16.dtb", /* Tentative. */
+	.dtb   = L"qcom\\x1p42100-lenovo-thinkbook-16.dtb",
 	.hwids = lenovo_thinkbook_16_g7_qoy_hwids,
 };
 DEVICE_DESC(lenovo_thinkbook_16_g7_qoy_dev);
