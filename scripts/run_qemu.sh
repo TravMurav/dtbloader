fd52e0228bc6671b1bfb092c6c2d85af3119ced3
Nikita Travkin <nikita@trvn.ru>
2025-11-08 10:13:00 +0500

    ci: Also boot test with default QEMU smbios

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 BASEDIR="$(dirname "$0")/../"
 BUILDDIR="$BASEDIR/build-aarch64"
@@ -19,6 +19,23 @@ then
 	wget https://github.com/pbatard/UEFI-Shell/releases/download/24H2/shellaa64.efi -O "$BUILDDIR/efi/boot/bootaa64.efi"
 fi
 
+QEMU_SMBIOS_ARGS=(
+	-smbios type=1,manufacturer='LENOVO',product='81F1',sku='LENOVO_MT_81F1_BU_idea_FM_Miix 630',family='Miix 630'
+	-smbios type=2,manufacturer='LENOVO',product='LNVNB161216'
+)
+
+if [ $# -ge 1 ]
+then
+	case $1 in
+		nosmbios)
+			QEMU_SMBIOS_ARGS=()
+			;;
+		miix)
+			# default
+			;;
+	esac
+fi
+
 timeout --foreground \
 	--kill-after=120s \
 	30s \
@@ -28,8 +45,7 @@ timeout --foreground \
 		-m 512 \
 		-bios "/usr/share/AAVMF/AAVMF_CODE.fd" \
 		-drive if=virtio,format=raw,file=fat:rw:"$BUILDDIR" \
-		-smbios type=1,manufacturer="LENOVO",product="81F1",sku="LENOVO_MT_81F1_BU_idea_FM_Miix 630",family="Miix 630" \
-		-smbios type=2,manufacturer="LENOVO",product="LNVNB161216" \
+		"${QEMU_SMBIOS_ARGS[@]}" \
 		-nographic -no-reboot \
 			| sed \
 				-e 's/\x1b\[[0-9]\+;01H/\n/g' \
